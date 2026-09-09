12542800be4ee4938228e0eb257f46d903a9ab30
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-09-09 11:43:56 -0600

    Mostrar mapa simple

--- app/map-simple/index.tsx
+++ app/map-simple/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 const MapScreen = () => {
   return (
@@ -16,7 +16,25 @@ const MapScreen = () => {
           longitudeDelta: 0.0421,
         }}
         style={styles.map}
-      />
+      >
+        <Marker
+          coordinate={{
+            latitude: 45.41256,
+            longitude: -75.698931,
+          }}
+          title="Aquí estoy"
+          description="Esta es mi casa"
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 45.434005,
+            longitude: -75.677708,
+          }}
+          title="Un parque"
+          description="Esta es mi casa"
+        />
+      </MapView>
     </View>
   );
 };
